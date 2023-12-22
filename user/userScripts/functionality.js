3ea85fd1cf82f32f3a26d9cf73b6bec53cfae6a0
Shubham0202 <shubham826852@gmail.com>
2023-12-22 20:09:34 +0530

    Create functionality.js

---
+++ user/userScripts/functionality.js
@@ -0,0 +1,7 @@
+"use strict"
+const closeErrors = document.querySelector('.close-error');
+closeErrors.addEventListener('click',()=>{
+  const accCommon = document.querySelector('.ac-common');
+  accCommon.style.display="none";
+});
+
