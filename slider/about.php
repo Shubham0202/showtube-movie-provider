df65aff255b22e243546d7c25eaaa082906fe123
Shubham0202 <shubham826852@gmail.com>
2023-12-28 21:37:27 +0530

    Create about.php

---
+++ slider/about.php
@@ -0,0 +1,76 @@
+<?php
+include '../connect.php';
+include '../loader.php';
+
+if(isset($_POST['report']))
+{
+    $userName= $_POST['userName'];
+    $email = $_POST['email'];
+    $suggestion = $_POST['suggestions'];
+
+    $sql= "insert into `feedbacks` (userName,email,suggestions) values('$userName','$email','$suggestion')";
+    $result= mysqli_query($con,$sql);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faster Movies</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/request.css">
+    <link rel="shortcut icon" href="../logo/logoAnd.png" type="image/x-icon">
+
+</head>
+
+<body>
+<div class="show-alert">
+        <?php include '../app.html'; ?>
+    </div>
+    <div class="show-web">
+    <nav class='navbar'>
+        <div class="hammer">
+            <span class="h  h1"></span>
+            <span class="h h2 "></span>
+            <span class="h h3 "></span>
+        </div>
+        <div class="appName">Show<span class="tube">Tube</span> </div>
+        <div class="searchBtn">
+            <i class='srch-btn fa fa-search'></i>
+        </div>
+    </nav>
+
+   
+    <!-- menu list -->
+    <div class="menu-list inactive-menu">
+        <ul class="ul-list">
+        <li><a class="list-items" href="https://freemovie.showtube.live"><i class="fas fa-home"></i> Home</a></li>
+            <li><a class="list-items" href="../slider/about.php"><i class="fas fa-info-circle"></i> About</a></li>
+            <li><a class="list-items" href="../slider/privacy.php"> <i class="fas fa-user-secret"></i> Privacy & Policy</a></li>
+            <li><a class="list-items" href="../slider/feedback.php"><i class="fas fa-comment"></i> Feedback</a></li>
+            <li><a class="list-items" href="../slider/shareapp.php"><i class="fas fa-share-alt"></i> Share App</a></li>
+         </ul>
+        <div class="close-menu inactive-close-menu"></div>
+    </div>
+    <!-- searchbar  -->
+    <div class="search-bar">
+        <input type="text" id="search" placeholder="search movies here" onkeyup="search()">
+    </div>
+    <!-- Main content  -->
+<main class="fr-container">
+
+</main>
+    </div>
+<script src="../scripts/main.js"></script>
+<script src="../scripts/checkRefer.js"></script>
+
+</body>
+
+</html>
